2d689823c30a52c221e5ab9a93434ca7087a9035
AnnaPoznanska <63655642+AnnaPoznanska@users.noreply.github.com>
2024-05-14 23:41:20 +0200

    dividing by zero error covered

--- MyCalculator2/Form1.cs
+++ MyCalculator2/Form1.cs
@@ -1,13 +1,15 @@
 using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MyCalculator2
 {
     public partial class Form1 : Form
     {
         Double resultValue = 0;
-        String operatorChosen = "";
+        System.String operatorChosen = "";
         bool isOperatorChosen = false;
         bool isSummedUp = false;
+        bool errorOccured = false;
 
         public Form1()
         {
@@ -28,90 +30,163 @@ namespace MyCalculator2
                 txtResult.Text = "";
                 resultValue = 0;
             }
+            // Clear the label above textbox to remove the div/0 error message
+            else if (errorOccured is true)
+            {
+                lblPendingVal.Text = "";
+            }
             // Get the button that was clicked
             Button button = (Button)sender;
             // Print text of the clicked button: add it any time it is clicked
             txtResult.Text = txtResult.Text + button.Text;
             isOperatorChosen = false;
             isSummedUp = false;
+            errorOccured = false;
+            btnPlus.BackColor = SystemColors.ControlLightLight;
+            btnMinus.BackColor = SystemColors.ControlLightLight;
+            btnMulti.BackColor = SystemColors.ControlLightLight;
+            btnDivide.BackColor = SystemColors.ControlLightLight;
+            btnDecimal.BackColor = SystemColors.ControlLightLight;
+            btnClearEntry.BackColor = SystemColors.ControlLight;
 
         }
 
         private void btnEquals_Click(object sender, EventArgs e)
         {
-            // Perform calculation depending on operator chosen
-            // Between the value after choosing operator, and the one after summing up
-            switch (operatorChosen)
+            if (errorOccured is true)
             {
-                case "+":
-                    txtResult.Text = (resultValue + Double.Parse(txtResult.Text)).ToString();
-                    break;
-                case "-":
-                    txtResult.Text = (resultValue - Double.Parse(txtResult.Text)).ToString();
-                    break;
-                case "*":
-                    txtResult.Text = (resultValue * Double.Parse(txtResult.Text)).ToString();
-                    break;
-                case "/":
-                    txtResult.Text = (resultValue / Double.Parse(txtResult.Text)).ToString();
-                    break;
-                default:
-                    break;
+                // perform 'click' event of the clear button
+                btnClear.PerformClick();
             }
+            else
+            {
+                // Perform calculation depending on operator chosen
+                // Between the value after choosing operator, and the one after summing up
+                switch (operatorChosen)
+                {
+                    case "+":
+                        txtResult.Text = (resultValue + Double.Parse(txtResult.Text)).ToString();
+                        break;
+                    case "-":
+                        txtResult.Text = (resultValue - Double.Parse(txtResult.Text)).ToString();
+                        break;
+                    case "*":
+                        txtResult.Text = (resultValue * Double.Parse(txtResult.Text)).ToString();
+                        break;
+                    case "/":
+                        // Avoid dividing by zero
+                        if (txtResult.Text == "0")
+                        {
+                            //txtResult.Text = "";
+                            resultValue = 0;
+                            lblPendingVal.Text = "Dividing by zero undefined";
+                            errorOccured = true;
+                            btnPlus.BackColor = SystemColors.ControlDark;
+                            btnMinus.BackColor = SystemColors.ControlDark;
+                            btnMulti.BackColor = SystemColors.ControlDark;
+                            btnDivide.BackColor = SystemColors.ControlDark;
+                            btnDecimal.BackColor = SystemColors.ControlDark;
+                            btnClearEntry.BackColor = SystemColors.ControlDark;
+                        }
+                        else
+                        {
+                            txtResult.Text = (resultValue / Double.Parse(txtResult.Text)).ToString();
+                        }
+                        //txtResult.Text = (resultValue / Double.Parse(txtResult.Text)).ToString();
+                        break;
+                    default:
+                        break;
+                }
             // Transfer the value in textbox to result
             resultValue = Double.Parse(txtResult.Text);
-            lblPendingVal.Text = "";
+            if (lblPendingVal.Text != "Dividing by zero undefined")
+                {
+                    lblPendingVal.Text = "";
+                }
             isSummedUp = true;
+            }
         }
 
         private void btnOperator_Click(object sender, EventArgs e)
         {
-            // Get the button that was clicked
-            Button button = (Button)sender;
-
-            // Sup up while choosing operator multiple times (without equal-to sign)
-            if (resultValue != 0 & isSummedUp is false)
+            if (errorOccured is true)
             {
-                // perform 'click' event of the equal-to button
-                btnEquals.PerformClick();
-                // Save information what operator was clicked
-                operatorChosen = button.Text;
-                // Print equation above textbox
-                lblPendingVal.Text = resultValue + " " + operatorChosen;
-                isOperatorChosen = true;
-                btnDecimal.BackColor = SystemColors.ControlLightLight;
+                // Do nothing
             }
             else
             {
-            // Save information what operator was clicked
-            operatorChosen = button.Text;
-            // Pass number stored in textbox to resultValue variable
-            resultValue = Double.Parse(txtResult.Text);
-            // Print equation above textbox
-            lblPendingVal.Text = resultValue + " " + operatorChosen;
-            isOperatorChosen = true;
-            btnDecimal.BackColor = SystemColors.ControlLightLight;
+                // Get the button that was clicked
+                Button button = (Button)sender;
+
+                // Sum up while choosing operator multiple times (without equal-to sign)
+                if (resultValue != 0 & isSummedUp is false)
+                {
+                    // Perform 'click' event of the equal-to button
+                    btnEquals.PerformClick();
+                    if (lblPendingVal.Text == "Dividing by zero undefined")
+                    {
+                        // Do nothing
+                    }
+                    else
+                    {
+                        // Save information what operator was clicked
+                        operatorChosen = button.Text;
+                        // Print equation above textbox
+                        lblPendingVal.Text = resultValue + " " + operatorChosen;
+                        isOperatorChosen = true;
+                        btnDecimal.BackColor = SystemColors.ControlLightLight;
+                    }
+                }
+                else
+                {
+                    // Save information what operator was clicked
+                    operatorChosen = button.Text;
+                    // Pass number stored in textbox to resultValue variable
+                    resultValue = Double.Parse(txtResult.Text);
+                    // Print equation above textbox
+                    lblPendingVal.Text = resultValue + " " + operatorChosen;
+                    isOperatorChosen = true;
+                    errorOccured = false;
+                    btnDecimal.BackColor = SystemColors.ControlLightLight;
+                }
             }
         }
 
         private void btnDecimal_Click(object sender, EventArgs e)
         {
-            // Get the button that was clicked
-            Button button = (Button)sender;
-            // Allow only one decimal point per value
-            if (txtResult.Text.Contains(button.Text) is false)
+            if (errorOccured is true)
             {
-                // Print text of the clicked button
-                txtResult.Text = txtResult.Text + button.Text;
-                btnDecimal.BackColor = SystemColors.ControlDarkDark;
-                isOperatorChosen = false;
+                // Do nothing
+            }
+            else
+            {
+                // Get the button that was clicked
+                Button button = (Button)sender;
+                // Allow only one decimal point per value
+                if (txtResult.Text.Contains(button.Text) is false)
+                {
+                    // Print text of the clicked button
+                    txtResult.Text = txtResult.Text + button.Text;
+                    btnDecimal.BackColor = SystemColors.ControlDarkDark;
+                    isOperatorChosen = false;
+                    errorOccured = false;
+                    isSummedUp = false;
+                }
             }
         }
 
         private void btnClearEntry_Click(object sender, EventArgs e)
         {
-            txtResult.Text = "0";
-            isOperatorChosen = false; //new
+            if (errorOccured is true)
+            {
+                // Do nothing
+            }
+            else
+            {
+                txtResult.Text = "0";
+                isOperatorChosen = false; //new
+                errorOccured = false;
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
@@ -122,6 +197,13 @@ namespace MyCalculator2
             isOperatorChosen = false; //new
             isSummedUp = false; //new
             operatorChosen = ""; //new
+            errorOccured = false;
+            btnPlus.BackColor = SystemColors.ControlLightLight;
+            btnMinus.BackColor = SystemColors.ControlLightLight;
+            btnMulti.BackColor = SystemColors.ControlLightLight;
+            btnDivide.BackColor = SystemColors.ControlLightLight;
+            btnDecimal.BackColor = SystemColors.ControlLightLight;
+            btnClearEntry.BackColor = SystemColors.ControlLight;
         }
     }
 }
